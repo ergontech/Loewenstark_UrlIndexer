1f023ea72d45c500577b6e6aff20122f75c6d76c
John jessee <subscriptions@oldstarhotel.com>
2016-01-18 14:58:30 -0800

    removed code that prevents indexing in magento 1.12. See https://github.com/occitech/Loewenstark_UrlIndexer/commit/e7bc8d02e0670c7e8bd11ded03b5bbf0289a10c4

--- app/code/community/Loewenstark/UrlIndexer/Model/Resource/Url.php
+++ app/code/community/Loewenstark/UrlIndexer/Model/Resource/Url.php
@@ -80,9 +80,7 @@ extends Mage_Catalog_Model_Resource_Url
         {
             $productIds = array($productIds);
         }
-        
-        if (empty($productIds)) return array(); // Skip on empty $productIds
-        
+
         if($this->_helper()->HideDisabledProducts($storeId) || $this->_helper()->HideNotVisibileProducts($storeId))
         {
             $products = parent::_getProducts($productIds, $storeId, $entityId, $lastEntityId);
