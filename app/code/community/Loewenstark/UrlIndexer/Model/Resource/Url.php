829d702275462f7eec4c33634336ad53a44b07ee
John jessee <subscriptions@oldstarhotel.com>
2016-01-21 15:27:49 -0800

    this should fix the config setting retreival and the actual index process

--- app/code/community/Loewenstark/UrlIndexer/Model/Resource/Url.php
+++ app/code/community/Loewenstark/UrlIndexer/Model/Resource/Url.php
@@ -76,41 +76,115 @@ extends Mage_Catalog_Model_Resource_Url
      */
     protected function _getProducts($productIds, $storeId, $entityId, &$lastEntityId)
     {
-        if(!is_array($productIds) && !is_null($productIds))
-        {
-            $productIds = array($productIds);
+        $products   = array();
+        $websiteId  = Mage::app()->getStore($storeId)->getWebsiteId();
+        $adapter    = $this->_getReadAdapter();
+
+
+        if ($productIds !== null) {
+            if (!is_array($productIds)) {
+                $productIds = array($productIds);
+            }
+        }
+        $bind = array(
+            'website_id'        => (int)$websiteId,
+            'entity_id'         => (int)$entityId
+        );
+        $select = $adapter->select()
+            ->useStraightJoin(true)
+            ->from(array('e' => $this->getTable('catalog/product')), array('entity_id'))
+            ->join(
+                array('w' => $this->getTable('catalog/product_website')),
+                'e.entity_id = w.product_id AND w.website_id = :website_id',
+                array()
+            )
+            ->where('e.entity_id > :entity_id')
+            ->order('e.entity_id')
+            ->limit($this->_productLimit);
+        if ($productIds !== null) {
+            $select->where('e.entity_id IN(?)', $productIds);
         }
 
-        if($this->_helper()->HideDisabledProducts($storeId) || $this->_helper()->HideNotVisibileProducts($storeId))
+        //if we are to ignore disabled products... add the necessary joins and conditions
+        if($this->_helper()->HideDisabledProducts($storeId))
         {
-            $products = parent::_getProducts($productIds, $storeId, $entityId, $lastEntityId);
-            $_attributes = array();
-            if($this->_helper()->HideDisabledProducts($storeId))
-            {
-                $_attributes[] = 'status';
+            $statusCode = Mage::getResourceModel('eav/entity_attribute')->getIdByCode('catalog_product', 'status');
+            $bind['status_id'] = (int)$statusCode;
+            $bind['disabled' ] = Mage_Catalog_Model_Product_Status::STATUS_DISABLED;
+            $bind['store_id'] = (int)$storeId;
+            $bind['default_store_id'] = 0;
+            $select->joinLeft(
+                array('s' => $this->getTable(array('catalog/product', 'int'))),
+                'e.entity_id = s.entity_id AND s.attribute_id = :status_id AND s.store_id = :store_id',
+                array()
+            );
+            $select->joinLeft(
+                array('ds' => $this->getTable(array('catalog/product', 'int'))),
+                'e.entity_id = ds.entity_id AND ds.attribute_id = :status_id AND ds.store_id = :default_store_id',
+                array()
+            );
+            $select->where('s.value <> :disabled OR (s.value IS NULL AND ds.value <> :disabled)');
+        }
+
+        //if we are to ignore not visible products... add the necessary joins and conditions
+        if($this->_helper()->HideNotVisibileProducts($storeId))
+        {
+            $visibilityCode = Mage::getResourceModel('eav/entity_attribute')->getIdByCode('catalog_product', 'visibility');
+            $bind['not_visible'] = Mage_Catalog_Model_Product_Visibility::VISIBILITY_NOT_VISIBLE;
+            $bind['visibility_id'] = (int)$visibilityCode;
+            $bind['store_id'] = (int)$storeId;
+            $bind['default_store_id'] = 0;
+
+            $select->joinLeft(
+                array('v' => $this->getTable(array('catalog/product', 'int'))),
+                'e.entity_id = v.entity_id AND v.attribute_id = :visibility_id AND v.store_id = :store_id',
+                array()
+            );
+            $select->joinLeft(
+                array('dv' => $this->getTable(array('catalog/product', 'int'))),
+                'e.entity_id = dv.entity_id AND dv.attribute_id = :visibility_id AND dv.store_id = :default_store_id',
+                array()
+            );
+            $select->where('v.value <> :not_visible OR (v.value IS NULL AND dv.value <> :not_visible)');
+        }
+
+
+        $rowSet = $adapter->fetchAll($select, $bind);
+        foreach ($rowSet as $row) {
+            $product = new Varien_Object($row);
+            $product->setIdFieldName('entity_id');
+            $product->setCategoryIds(array());
+            $product->setStoreId($storeId);
+            $products[$product->getId()] = $product;
+            $lastEntityId = $product->getId();
+        }
+
+        unset($rowSet);
+
+        if ($products) {
+            $select = $adapter->select()
+                ->from(
+                    $this->getTable('catalog/category_product'),
+                    array('product_id', 'category_id')
+                )
+                ->where('product_id IN(?)', array_keys($products));
+            $categories = $adapter->fetchAll($select);
+            foreach ($categories as $category) {
+                $productId = $category['product_id'];
+                $categoryIds = $products[$productId]->getCategoryIds();
+                $categoryIds[] = $category['category_id'];
+                $products[$productId]->setCategoryIds($categoryIds);
             }
-            if($this->_helper()->HideNotVisibileProducts($storeId))
-            {
-                $_attributes[] = 'visibility';
-            }
-            foreach ($_attributes as $attributeCode) {
+
+            foreach (array('name', 'url_key', 'url_path') as $attributeCode) {
                 $attributes = $this->_getProductAttribute($attributeCode, array_keys($products), $storeId);
                 foreach ($attributes as $productId => $attributeValue) {
-                    if(($attributeCode == 'status' && $attributeValue == Mage_Catalog_Model_Product_Status::STATUS_DISABLED)
-                       ||
-                       ($attributeCode == 'visibility' && $attributeValue == Mage_Catalog_Model_Product_Visibility::VISIBILITY_NOT_VISIBLE)
-                      )
-                    {
-                        if(isset($productIds[$productId]))
-                        {
-                            unset($productIds[$productId]);
-                        }
-                    }
+                    $products[$productId]->setData($attributeCode, $attributeValue);
                 }
             }
-            return $products;
         }
-        return parent::_getProducts($productIds, $storeId, $entityId, $lastEntityId);
+
+        return $products;
     }
 
     /**
